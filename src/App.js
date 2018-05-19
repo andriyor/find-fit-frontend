0e46d00b6296a94ce3172de1e606a2b6782f7a25
BilousAlexandr <bilousbrainbasket@gmail.com>
2018-05-19 22:03:42 +0300

    add eventInfo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ControlledOpenSelect from './ControlledOpenSelect';
 import DateAndTimePickers from './DateAndTimePickers'
 import InputAdornments from './InputAdornments'
 import LoginAuthorization from './LoginAuthorization'
+import EventInfo from './EventInfo'
 
 
 // import RadioButtonsGroup from './RadioButtonsGroup';
@@ -16,22 +17,25 @@ import { connect } from 'react-redux'
 class App extends React.Component {
   render() {
 
-    const ages = [{key: 1, value: 10, text: 'Ten-twenty'}, 
-                  {key: 2, value: 20, text: 'Twenty-thirty'}, 
-                  {key: 3, value: 30, text: 'Ten-twenty'}];
+    // const ages = [{key: 1, value: 10, text: 'Ten-twenty'},
+    //               {key: 2, value: 20, text: 'Twenty-thirty'},
+    //               {key: 3, value: 30, text: 'Ten-twenty'}];
 
     const sex = [{key: 1, value: 1, text: 'male'}, {key: 2, value: 2, text: 'female'}]
 
     return (
       <MiniDrawer>
         <div>
+            <div style={{position: 'absolute', marginTop: '100px', right: "30px", float: 'right', zIndex: '99999', background: "white"}}>
+                <EventInfo/>
+            </div>
             <div style={{position: 'absolute', marginTop: '100px', marginLeft: '30px', background: 'white', zIndex: '999999', maxWidth: '215px', padding: '20px 30px'}}>
                 <div style={{fontSize: '20pt'}}>Create your event</div>
                 <InputAdornments/>
                 <DateAndTimePickers style={{marginLeft: '20px'}}/>
             </div>
             <div style={{display: 'flex'}}>
-                <ControlledOpenSelect title='Age' items={ages}/>
+                {/*<ControlledOpenSelect title='Age' items={ages}/>*/}
                 <ControlledOpenSelect title='Sex' items={sex}/>
                 <DateAndTimePickers label='Start'/>
                 <DateAndTimePickers label='End'/>
---
+++ src/EventInfo.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const styles = theme => ({
+    card: {
+        maxWidth: 400,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    actions: {
+        display: 'flex',
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+        marginLeft: 'auto',
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+});
+
+class EventInfo extends React.Component {
+    state = { expanded: false };
+
+    handleExpandClick = () => {
+        this.setState({ expanded: !this.state.expanded });
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div>
+                <Card className={classes.card}>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label="Recipe" className={classes.avatar}>
+                                UR
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton>
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title="Shrimp and Chorizo Paella"
+                        subheader="September 14, 2016"
+                    />
+                    <CardMedia
+                        className={classes.media}
+                        image="/home/alexandr/Desktop/run-21.jpg"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography component="p">
+                           Hi, i`m find some people for morning
+                        </Typography>
+                    </CardContent>
+                    <CardActions className={classes.actions} disableActionSpacing>
+                        <FormControlLabel
+                            control={
+                                <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} value="checkedH" />
+                            }
+                            label="You and more"
+                        />
+                        <IconButton aria-label="Share">
+                            <ShareIcon />
+                        </IconButton>
+                        <IconButton
+                            className={classnames(classes.expand, {
+                                [classes.expandOpen]: this.state.expanded,
+                            })}
+                            onClick={this.handleExpandClick}
+                            aria-expanded={this.state.expanded}
+                            aria-label="Show more"
+                        >
+                            <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                    <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph variant="body2">
+                                Lorem ipsum
+                            </Typography>
+                            <Typography paragraph>
+                                Lorem ipsum
+                            </Typography>
+                            <Typography paragraph>
+                                Lorem ipsum
+                            </Typography>
+                            <Typography paragraph>
+                                Lorem ipsum
+                            </Typography>
+                            <Typography>
+                                Lorem ipsum
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+            </div>
+        );
+    }
+}
+
+EventInfo.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(EventInfo);
